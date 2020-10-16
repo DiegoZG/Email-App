f2ef355a94ab8f46005acf1b244698f8710bfaba
Diwo777 <diego.zegarra79@icloud.com>
2020-10-16 18:27:18 -0400

    added react

---
+++ playground.js
@@ -0,0 +1,14 @@
+// function fetchAlbums() {
+//     fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//     .then( res => res.json() )
+//     .then( data => console.log(data))
+// }
+
+
+// const fetchAlbums = async () => {
+//     const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//     const json = await res.json() 
+//     console.log(json)
+// }
+
+// fetchAlbums();
